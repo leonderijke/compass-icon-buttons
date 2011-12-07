9c09935a383b1ab7158a77b06d318f54fb220b53
Leon de Rijke <leon@cmsworks.nl>
2011-12-07 10:49:41 +0100

    Add project files

--- compass-icon-buttons/templates/project/manifest.rb
+++ compass-icon-buttons/templates/project/manifest.rb
@@ -1,3 +1,14 @@
-# Make sure you list all the project template files here in the manifest.
+description "buttons with sprited icons, create single line or multi line buttons with ease."
+
 stylesheet 'buttons.scss', :media => 'screen, projection'
 html 'buttons.html
+
+help %Q{
+Installs some html, a stylesheet partial and some images that
+you can use directly or refer to as an example.
+}
+
+welcome_message %Q{
+In buttons.html you'll find some example markup for the icon buttons. In the stylesheet partial you can see how to
+use the library and apply it to your markup.
+}
